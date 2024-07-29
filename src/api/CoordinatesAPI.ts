d7cfa3ddc1dbd6bae90e404e26cca36d5ddd3113
Johan <johan.brun2@gmail.com>
2024-07-29 23:14:38 +0200

    Add search for coordinates

---
+++ src/api/CoordinatesAPI.ts
@@ -0,0 +1,18 @@
+import axios from "axios"
+import { Coordinates } from "../types/Coordinates"
+
+export const getCoordinates = async (place: string): Promise<Coordinates> => {
+    const peliasResults = await axios.get(`http://pelias.smappen.com:4000/v1/search?text=${place}`)
+    if(peliasResults.status === 200) {
+        const data = peliasResults.data
+        const coordinates = data.features[0].geometry.coordinates
+        const formatedCoordinates: Coordinates = {
+            lat: coordinates[0],
+            long: coordinates[1]
+        }
+        return formatedCoordinates
+    }
+    else {
+        throw new Error(`Pelias error! Statuscode : ${peliasResults.status}, error message : ${peliasResults.statusText}`)
+    }
+}
--- src/components/MeteoResults.tsx
+++ src/components/MeteoResults.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react"
+import { Coordinates } from "../types/Coordinates"
+import { getCoordinates } from "../api/CoordinatesAPI"
 
 interface IMeteoResults {
     searchText: string
@@ -5,9 +8,29 @@ interface IMeteoResults {
 
 export const MeteoResults: React.FC<IMeteoResults> = (props) => {
 
-    return(
-        <>
-        This is search for {props.searchText}
-        </>
-    )
+    const [coordinates, setCoordinates] = useState<Coordinates | undefined>()
+
+    useEffect(()=> {
+        console.log('trigger!')
+        const getData = async () => {
+            const data = await getCoordinates(props.searchText)
+            setCoordinates(data)
+        }
+
+        if(props.searchText) {
+            getData()
+            .catch(console.error)   
+        }
+    },[props.searchText])
+
+    if(coordinates) {
+        return(
+            <>
+            lat is {coordinates.lat} <br/>
+            long is {coordinates.long}
+            </>
+        )
+    } else {
+        return <>No coordinates yet!</>
+    }
 }
